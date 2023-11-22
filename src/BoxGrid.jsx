9933bc10e2dc16457503c63c465c20d326bdccd2
Ricardo <ricardobozzi23@gmail.com>
2023-11-22 20:20:06 +0100

    Refactored. Began 'onclick' functionality that changes colour of clicked box and checks of each box has same colour

--- src/BoxGrid.jsx
+++ src/BoxGrid.jsx
@@ -1,3 +1,5 @@
+import {useState} from 'react';
+import {v4 as uuid} from 'uuid';
 import ColouredBox from './ColouredBox';
 import './BoxGrid.css';
 
@@ -8,25 +10,90 @@ const randomColor = () => {
     return `rgb(${r}, ${g}, ${b})`;
 }
 
+const randomIdx = (arr) => {
+    const idx = Math.floor(Math.random() * arr.length);
+         return arr[idx];
+}
+
+
 export default function BoxGrid(){
-    const boxes = []
-    const numBoxes = 10
+    // const boxes = [];
+    // cost numBoxres = 10;
 
-    const colorsArr = [];
-    const numColors = 5;
+    const numBoxes = 6;
+    const numColors = 2;
+    // const colorsArr = [];
 
+    
    
+  const generateColors = () => {
+    let colorsArr = []
     for (let i =0; i < numColors; i++) {
         colorsArr.push(randomColor());
+       }
+  return colorsArr;
+}
+
+
+
+    const generateBoxes = () => {
+      let boxes = [];
+        let box;
+        for (let i = 0; i < numBoxes; i++) {
+        box = {
+            id: uuid(),
+            background: randomIdx(colorsArr)
+          }
+          boxes.push(box);
+        }
+        return boxes;
+        
     }
 
-    console.log(colorsArr)
 
-    for (let i = 0; i < numBoxes; i++) {
-        boxes.push(<ColouredBox colorsArr={colorsArr}/>)
+
+
+    const [colorsArr, setColorsArr] = useState(generateColors);
+    const [boxArr, setBoxArr] = useState(generateBoxes);
+    const [endGame, setEndGame] = useState(false);
+  
+
+
+    const onClick = (id) => {
+        setBoxArr((prevBoxArr) => {
+           return prevBoxArr.map((box) => {
+            if (id === box.id) {
+                return {...box, background: randomIdx(colorsArr)}
+            } else {return box}
+           })
+        })
+        if (runValidate(boxArr)) {
+            setEndGame(!endGame);
+        }
+        // boxArr.map((box) => {
+        //     if (box.id === id) {
+        //         setBoxArr
+        //     } else {console.log('moving on')}
+        // })
     }
+
+    const runValidate = (boxArr) => {
+        return boxArr.every((box) => box.background === boxArr[0].background);
+    }
+   
 
     return (
-        <div className="box-grid">{boxes}</div>
+        !endGame ? 
+        <div className="box-grid">
+            {boxArr.map((box) => (
+                <ColouredBox
+                key={box.id}
+                backgroundColor={box.background}
+                changeColour={() => onClick(box.id)}
+                />
+            ))}
+        </div>
+        : <h1>You Won</h1>
+
     )
 }
--- src/ColouredBox.jsx
+++ src/ColouredBox.jsx
@@ -1,21 +1,22 @@
 import {useState} from 'react';
 import './ColouredBox.css';
 
-const randomChoice = (arr) => {
-     const idx = Math.floor(Math.random() * arr.length);
-          return arr[idx];
- }
+// const randomChoice = (arr) => {
+//      const idx = Math.floor(Math.random() * arr.length);
+//           return arr[idx];
+//  }
 
 
 
 
-export default function ColouredBox({colorsArr}) {
-   const [backgroundColor, setBackgroundColor] = useState(randomChoice(colorsArr));
+export default function ColouredBox({backgroundColor, changeColour}) {
+//    const [backgroundColor, setBackgroundColor] = useState(randomChoice(colorsArr));
 
-   const changeColor = () => {
-setBackgroundColor(randomChoice(colorsArr));
-   }
+//    const changeColor = () => {
+// setBackgroundColor(randomChoice(colorsArr));
+//    }
+
     return(
-        <div className="box" onClick={changeColor} style={{backgroundColor: backgroundColor}}></div>
+        <div className="box"  style={{backgroundColor: backgroundColor}} onClick={changeColour}></div>
     )
 }
