eff5b61bc97a480fcb20d6c96999bd71649623a0
Ricardo <ricardobozzi23@gmail.com>
2023-11-29 18:48:19 +0100

    removed comments

--- src/ColouredBox.jsx
+++ src/ColouredBox.jsx
@@ -1,21 +1,11 @@
 import {useState} from 'react';
 import './ColouredBox.css';
 
-// const randomChoice = (arr) => {
-//      const idx = Math.floor(Math.random() * arr.length);
-//           return arr[idx];
-//  }
 
 
 
 
 export default function ColouredBox({backgroundColor, changeColour}) {
-//    const [backgroundColor, setBackgroundColor] = useState(randomChoice(colorsArr));
-
-//    const changeColor = () => {
-// setBackgroundColor(randomChoice(colorsArr));
-//    }
-
     return(
         <div className="box"  style={{backgroundColor: backgroundColor}} onClick={changeColour}></div>
     )
